e7ad5fa2376b417b60a9b043f716079682c66d33
Falak Amin <falakamin2000@gmail.com>
2021-09-29 18:59:41 +0500

    Created date formatting utility

--- src/components/Main/Form/Form.js
+++ src/components/Main/Form/Form.js
@@ -7,14 +7,14 @@ import { ExpenseTrackerContext } from '../../../context/context';
 
 import {v4 as uuidv4} from "uuid";
 import { incomeCategories, expenseCategories } from '../../../constants/categories';
-
+import formDate from '../../../utils/formaDate';
 
 
 const initialState ={
     amount :"",
     category :"",
     type:"Income",
-    date:new Date(),
+    date: formDate(new Date()),
 }
 const Form = () => {
     const classes = useStyles();
@@ -60,7 +60,7 @@ const Form = () => {
                     <TextField type="number" label="Amount" fullWidth value={formData.amount} onChange={(e)=> setFormData({ ...formData, amount :e.target.value})}/>
                 </Grid>
                 <Grid item xs={6}>
-                    <TextField type="date" label="Date" fullWidth value={formData.date} onChange={(e)=> setFormData({ ...formData, date :e.target.value})}/>
+                    <TextField type="date" label="Date" fullWidth value={formData.date} onChange={(e)=> setFormData({ ...formData, date :formDate(e.target.value)})}/>
                 </Grid>
                 <Button className={classes.button} variant="outlined" color="primary" fullWidth onClick={createTransaction}>
                     Create
