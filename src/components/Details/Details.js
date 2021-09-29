0a99b91c9686d011e6866c964d3f3b327d3f7431
Falak Amin <falakamin2000@gmail.com>
2021-09-29 19:47:41 +0500

    Created useTransactions hook

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -3,15 +3,22 @@ import React from 'react'
 import { Card,CardContent, CardHeader,Typography } from '@material-ui/core'
 import { Doughnut } from 'react-chartjs-2';
 import useStyles  from './style';
+import {useTransactions} from "../../hooks/useTransactions"
 
 const Details = ({title}) => {
     const classes =useStyles();
+    const { total, chartData } = useTransactions(title);
+
     return (
-        <Card className={title === "Income" ? classes.income :classes.expense}>
-            <CardHeader title ={title}/>
+        <Card className={title === 'Income'? classes.income: classes.expense}>
+            <CardHeader title={title}/>
             <CardContent>
-                <Typography variant="h5">$50</Typography>
-                {/* <Doughnut data="Data"/> */}
+                {/* <Typography variant="h5">$ {total}</Typography>
+                {
+                    total>0 && <Doughnut data={chartData}/>
+                } */}
+                  <Typography variant="h5">${total}</Typography>
+                <Doughnut data={chartData}/>
             </CardContent>
         </Card>
     )
